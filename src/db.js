834f876f2cf7f508f8b29523bf6eb763a1fba764
SEMİH İŞBAŞ <sisbas@yahoo.com>
2025-10-02 11:30:38 +0300

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,1 +1,14 @@
-export const students = [];
+// src/db.js
+require('dotenv').config();
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false // Neon ile uyumlu
+  }
+});
+
+module.exports = {
+  query: (text, params) => pool.query(text, params)
+};
